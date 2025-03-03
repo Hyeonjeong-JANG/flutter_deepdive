2d33540ecb39bb5b73662c0654028dc8c34bb8b8
HyeonjeongJang <hyeonjeong.jang.0302@gmail.com>
2025-03-03 13:51:55 +0900

    init & login screen

---
+++ lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,54 @@
+import 'package:actual/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String> onChanged;
+
+  const CustomTextFormField({
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autofocus = false,
+    required this.onChanged,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INOUT_BODER_COLOR,
+        width: 1.0,
+      ),
+    );
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      // 비밀번호 입력할 때
+      obscureText: obscureText,
+      autofocus: autofocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        filled: true, // 배경색 채우기
+        border: baseBorder, // 모든 Input 상태이 기본 스타일 세팅
+        enabledBorder: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ),
+    );
+  }
+}
